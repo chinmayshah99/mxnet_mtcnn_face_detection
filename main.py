28617a630757f9ccd7db6cf9c08c0e17c74d503e
Chinmay Shah <chinmayshah3899@gmail.com>
2019-05-27 12:55:35 +0530

    added video support

--- main.py
+++ main.py
@@ -9,68 +9,74 @@ detector = MtcnnDetector(model_folder='model', ctx=mx.gpu(), num_worker = 4 , ac
 
 
 
-def main_logic(img_name):
-    print(img_name)
-    img = cv2.imread(img_name)
-    temp_img_name = img_name.rsplit('.', 1)
-    print(temp_img_name)
-    # run detector
-    results = detector.detect_face(img)
+def main_logic(file_name):
+    # Find OpenCV version
+    (major_ver, minor_ver, subminor_ver) = (cv2.__version__).split('.')
+
+    print(file_name)
+    temp_file_name = file_name.rsplit('.', 1)[0]
+    print(temp_file_name)
+
     opdirname = 'out/'
 
-    if results is not None:
+    camera = cv2.VideoCapture('inp/' + file_name)
+    print(camera.isOpened()) # checking if file is opened
 
-        total_boxes = results[0]
-        points = results[1]
+    if camera.isOpened():
+
+    
+     
+        # With webcam get(CV_CAP_PROP_FPS) does not work.
+        # Let's see for ourselves.
         
-        # extract aligned face chips
-        chips = detector.extract_image_chips(img, points, 144, 0.37)
-        for i, chip in enumerate(chips):
-            # cv2.imshow('chip_'+str(i), chip)
-            print(os.path.join(opdirname,'chip_'+ temp_img_name+str(i)+'.png'))
-            cv2.imwrite(os.path.join(opdirname,'chip_'+ temp_img_name+str(i)+'.png'), chip)
+        if int(major_ver)  < 3 :
+            fps = int(camera.get(cv2.cv.CV_CAP_PROP_FPS))
+        else :
+            fps = int(camera.get(cv2.CAP_PROP_FPS))
 
-        draw = img.copy()
-        for b in total_boxes:
-            cv2.rectangle(draw, (int(b[0]), int(b[1])), (int(b[2]), int(b[3])), (255, 255, 255))
+        no_frames = int(camera.get(5)) # CV_CAP_PROP_FRAME_COUNT
 
-        for p in points:
-            for i in range(5):
-                cv2.circle(draw, (p[i], p[i + 5]), 1, (0, 0, 255), 2)
-        print(os.path.join(opdirname,'chip_draw'+temp_img_name+str(i)+'.png'))
-        cv2.imwrite(os.path.join(opdirname,'chip_draw'+temp_img_name+str(i)+'.png'), draw)
-        # cv2.imshow("detection result", draw)
-        cv2.waitKey(0)
+        next_frame = 0
+        for i in range(no_frames):
+            camera.set(1, next_frame) #CV_CAP_PROP_POS_FRAMES
+            next_frame += fps # get frame every 1s
+            
+            grab, frame = camera.read()
+            img = cv2.resize(frame, (320,180)).copy()
 
-    # --------------
-    # test on camera
-    # --------------
-    '''
-    camera = cv2.VideoCapture(0)
-    while True:
-        grab, frame = camera.read()
-        img = cv2.resize(frame, (320,180))
+            t1 = time.time()
+            results = detector.detect_face(img)
 
-        t1 = time.time()
-        results = detector.detect_face(img)
-        print 'time: ',time.time() - t1
+            if results is not None:
 
-        if results is None:
-            continue
+                total_boxes = results[0]
+                points = results[1]
+                
+                # extract aligned face chips
+                chips = detector.extract_image_chips(img, points, 144, 0.37)
+                for i, chip in enumerate(chips):
+                    # cv2.imshow('chip_'+str(i), chip)
+                    print(os.path.join(opdirname,'chip_'+ temp_file_name+str(i)+str(t1) +'.png'))
+                    cv2.imwrite(os.path.join(opdirname,'chip_'+ temp_file_name+str(i)+str(t1) +'.png'), chip)
 
-        total_boxes = results[0]
-        points = results[1]
+                draw = img.copy()
+                for b in total_boxes:
+                    cv2.rectangle(draw, (int(b[0]), int(b[1])), (int(b[2]), int(b[3])), (255, 255, 255))
 
-        draw = img.copy()
-        for b in total_boxes:
-            cv2.rectangle(draw, (int(b[0]), int(b[1])), (int(b[2]), int(b[3])), (255, 255, 255))
+                for p in points:
+                    for i in range(5):
+                        cv2.circle(draw, (p[i], p[i + 5]), 1, (0, 0, 255), 2)
+                print(os.path.join(opdirname,'chip_draw'+temp_file_name+str(i)+str(t1) +'.png'))
+                cv2.imwrite(os.path.join(opdirname,'chip_draw'+temp_file_name+str(i)+str(t1) +'.png'), draw)
 
-        for p in points:
-            for i in range(5):
-                cv2.circle(draw, (p[i], p[i + 5]), 1, (255, 0, 0), 2)
-        cv2.imshow("detection result", draw)
-        cv2.waitKey(30)
-    '''
+                if no_frames == next_frame:
+                    break
+
+    else:
+        assert "couldn't open file"
+
+
+
 for root, dirs, files in os.walk("inp"):  
     for filename in files:
         main_logic(filename)
